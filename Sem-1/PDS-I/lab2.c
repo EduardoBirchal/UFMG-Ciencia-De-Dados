6ad54f21e79698936548a68f0329939c94276e01
Eduardo Birchal <edubirchal@gmail.com>
2024-03-23 21:49:10 -0300

    ADD: lab2.c

---
+++ Sem-1/PDS-I/lab2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char **argv) {
+    int foo, bar;
+
+    scanf ("%d %d", &foo, &bar);
+    printf("%d\n%d", foo, bar);
+
+    return 0;
+}
